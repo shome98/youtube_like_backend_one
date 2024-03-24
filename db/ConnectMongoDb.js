a630bdf7aab41bced312178750b84a1da943e84b
Sowparna Shome <soupornoshome@gmail.com>
2024-03-24 11:16:55 +0530

    connection established with mongodb

--- db/ConnectMongoDb.js
+++ db/ConnectMongoDb.js
@@ -1,1 +1,13 @@
-//connects with mongodb goes here
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDatabase = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MONGODB connected!!! DB HOST: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("MONGODB CONNECTION ERROR!!! ", error)
+        process.exit(1)
+    }
+}
+export default connectDatabase
